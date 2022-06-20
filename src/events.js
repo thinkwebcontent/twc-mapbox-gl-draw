e8fd398a80e5e214583b4d8061ba88f16b4dbea7
Rhys <r.stubbs@thinkspain.com>
2022-06-20 14:20:58 +0200

    do not try and prevent events

--- src/events.js
+++ src/events.js
@@ -86,7 +86,18 @@ export default function(ctx) {
       time: new Date().getTime(),
       point: event.point
     };
+
     const target = featuresAt.touch(event, null, ctx)[0];
+
+    // If there are no mapbox targets nearby, let the event propagate through
+    if (!target && currentModeName === 'simple_select') {
+      return;
+    }
+
+    // Prevent emulated mouse events because we will fully handle the touch here.
+    // This does not stop the touch events from propogating to mapbox though.
+    // event.originalEvent.preventDefault();
+
     event.featureTarget = target;
     currentMode.touchstart(event);
   };
@@ -106,7 +117,14 @@ export default function(ctx) {
     }
 
     const target = featuresAt.touch(event, null, ctx)[0];
+
+    // If there are no mapbox targets nearby, let the event propagate through
+    if (!target && currentModeName === 'simple_select') {
+      return;
+    }
+
     event.featureTarget = target;
+
     if (isTap(touchStartInfo, {
       time: new Date().getTime(),
       point: event.point
